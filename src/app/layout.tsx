70bac29d279099aff7cbf6714155cc2cda5c348e
ismail <coderismail22@gmail.com>
2024-03-17 16:01:19 +0600

    stars-background-added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    // Stars Background 
     <html lang="en">
       <body className={inter.className}>
         <StarsCanvas />
